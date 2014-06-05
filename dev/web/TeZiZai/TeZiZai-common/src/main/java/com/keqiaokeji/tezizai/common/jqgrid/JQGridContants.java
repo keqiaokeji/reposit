2367b0e1c4fa2881921a1162cb54cbd2e4ee9fde
songhaikang <songhaikang@163.com>
2014-06-05 14:30:38 +0800

    优化项目

---
+++ dev/web/TeZiZai/TeZiZai-common/src/main/java/com/keqiaokeji/tezizai/common/jqgrid/JQGridContants.java
@@ -0,0 +1,73 @@
+package com.keqiaokeji.tezizai.common.jqgrid;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: keqiaokeji
+ * Date: 14-5-9
+ * Time: 下午6:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class JQGridContants {
+
+    /**
+     * 编辑操作中新增的标识
+     */
+    public final static String EDIT_OPER_ADD = "add";
+
+    /**
+     * 编辑操作中删除的标识
+     */
+    public final static String EDIT_OPER_DEL = "del";
+
+    /**
+     * 编辑操作中修改的标识
+     */
+    public final static String EDIT_OPER_UPDATE = "edit";
+
+
+    /**
+     * 升序
+     */
+    public final static String ORDER_BY_ASC= "asc";
+
+    /**
+     * 降序
+     */
+    public final static String ORDER_BY_DESC= "desc";
+
+
+    /**
+     * 条件之间是and关系
+     */
+    public final static String FILTER_GROUPOPS_AND = "AND";
+
+    /**
+     * 条件之间是or关系
+     */
+    public final static String FILTER_GROUPOPS_OR = "OR";
+
+
+    /**
+     * 条件为等于
+     */
+    public final static String FILTER_OPER_EQ = "eq";
+
+    /**
+     * 条件为不等于
+     */
+    public final static String FILTER_OPER_NE = "ne";
+
+    /**
+     * 条件为包含
+     */
+    public final static String FILTER_OPER_CN = "cn";
+
+    /**
+     * 条件为不包含
+     */
+    public final static String FILTER_OPER_NC = "nc";
+
+
+
+
+}
